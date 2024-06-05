cee5706f7e983656a51b5a359ca73644ee4f261e
Himmat20 <159880038+Himmat20@users.noreply.github.com>
2024-06-05 10:05:28 +0530

    Add files via upload

---
+++ Untitled (2).py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().system('pip install notebook')
+
+
+# In[ ]:
+
+
+# import random
+
+# # Defining responses
+# greetings = ['hello', 'hi', 'hey', 'howdy', 'hola']
+# responses = {
+#     'what is your name?': 'My name is CD_bot.',
+#     'how are you?': 'I am doing well, thank you!',
+#     'default': 'Sorry, I did not understand your question.',
+#     'who are you?':'I am a chat bot created by Anmol.'
+    
+# }
+
+# # greet the user
+# def greet():
+#     print("ChatBot: " + random.choice(greetings))
+
+# # Function to respond to user inputw
+# def respond(user_message):
+#     if user_message.lower() in responses:
+#         print("ChatBot: " + responses[user_message.lower()])
+#     else:
+#         print("ChatBot: " + responses['default'])
+
+# # Main function to interact with the user
+# def chat():
+#     print("ChatBot: Hello! I am a simple chatbot. How can I assist you today?")
+#     while True:
+#         user_input = input("You: ")
+#         if user_input.lower() == 'exit':
+#             print("ChatBot: Goodbye! Have a great day.")
+#             break
+#         greet()
+#         respond(user_input)
+
+# # Start
+# chat()
+
+
+# In[ ]:
+
+
+
+
